65d14abdadde1c0e5cec6cba914bc94fb3d46aa4
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-05-28 12:42:03 -0600

    Se agregan regiones

---
+++ Northwind/BackEND/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEND.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private ICategoryDAL categoryDAL;
+
+        #region Constructores
+        public CategoryController()
+        {
+                
+        }
+
+        #endregion
+        #region Consultas
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        #endregion
+        #region Agregar
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        #endregion
+        #region Modificar
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        #endregion
+        #region Eliminar
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        #endregion
+    }
+}
