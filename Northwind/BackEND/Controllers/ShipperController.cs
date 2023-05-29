54f180c66b6f0d857edd66f192288f8feeb65abc
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-05-29 10:40:20 -0600

    Implement Shipper Datas Homework Week2

---
+++ Northwind/BackEND/Controllers/ShipperController.cs
@@ -0,0 +1,61 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEND.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipperController : ControllerBase
+    {
+        private IShipperDAL shipperDAL;
+
+        #region Constructores
+        public ShipperController()
+        {
+            shipperDAL = new ShipperDALImpl();
+        }
+        #endregion
+        #region Consultas
+        // GET: api/<ShipperController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Shipper> categories = shipperDAL.GetAll();
+            return new JsonResult(categories);
+        }
+
+        // GET api/<ShipperController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Shipper shipper = shipperDAL.Get(id);
+            return new JsonResult(shipper);
+        }
+        #endregion
+        #region Agregar
+        // POST api/<ShipperController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        #endregion
+        #region Modificar
+        // PUT api/<ShipperController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        #endregion
+        #region Eliminar
+        // DELETE api/<ShipperController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        #endregion
+    }
+}
---
+++ Northwind/DAL/Implementations/ShipperDALImpl.cs
@@ -0,0 +1,77 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class ShipperDALImpl : IShipperDAL
+    {
+        private NorthWindContext _northWindContext;
+        private UnidadDeTrabajo<Shipper> unidad;
+        #region Add
+        public bool Add(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Find
+        public IEnumerable<Shipper> Find(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region get
+        public Shipper Get(int id)
+        {
+            Shipper Shipper = null;
+            using (unidad = new UnidadDeTrabajo<Shipper>(new NorthWindContext()))
+            {
+                Shipper = unidad.genericDAL.Get(id);
+            }
+            return Shipper;
+        }
+        public IEnumerable<Shipper> GetAll()
+        {
+            IEnumerable<Shipper> categories = null;
+            using (unidad = new UnidadDeTrabajo<Shipper>(new NorthWindContext()))
+            {
+                categories = unidad.genericDAL.GetAll();
+            }
+            return categories;
+        }
+        #endregion
+        #region Remove
+        public bool Remove(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Single
+        public Shipper SingleOrDefault(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Update
+        public bool Update(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
