52a78cbee82334e5232d494d9307044dd6fde0a3
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-06-04 14:49:32 -0600

    ShipperHelper GetALL

---
+++ Northwind/FrontEND/Helpers/ShipperHelper.cs
@@ -0,0 +1,30 @@
+﻿using FrontEND.Models;
+using Newtonsoft.Json;
+using NuGet.Protocol.Core.Types;
+
+namespace FrontEND.Helpers
+{
+    public class ShipperHelper
+    {
+        ServiceRepository repository;
+        public ShipperHelper()
+        {
+            repository = new ServiceRepository();
+        }
+        public List<ShipperViewModel> GetAll()
+        {
+
+            List<ShipperViewModel> lista = new List<ShipperViewModel>();
+
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Shipper/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ShipperViewModel>>(content);
+            }
+
+            return lista;
+        }
+
+    }
+}
---
+++ Northwind/FrontEND/Models/ShipperViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FrontEND.Models
+{
+    public class ShipperViewModel
+    {
+        public int ShipperId { get; set; }
+        public string CompanyName { get; set; } = null!;
+    }
+}
