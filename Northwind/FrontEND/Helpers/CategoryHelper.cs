0057dd075bf5b164734f27e9f765329c9786a0a7
HansAbarca <68607913+HansAbarca@users.noreply.github.com>
2023-06-04 10:04:48 -0600

    Category Helper GetAll

---
+++ Northwind/FrontEND/Helpers/CategoryHelper.cs
@@ -0,0 +1,29 @@
+﻿using FrontEND.Models;
+using Newtonsoft.Json;
+
+namespace FrontEND.Helpers
+{
+    public class CategoryHelper
+    {
+        ServiceRepository repository;
+        public CategoryHelper()
+        {
+            repository = new ServiceRepository();
+        }
+
+        public List<CategoryViewModel> GetAll()
+        {
+
+            List<CategoryViewModel> lista = new List<CategoryViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Category/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<CategoryViewModel>>(content);
+            }
+
+            return lista;
+        }
+
+    }
+}
